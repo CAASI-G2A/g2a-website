5a62cd5c95c670db72bc3b754e7a597fc9ee94ea
Connor Kalina <cjk94@pitt.edu>
2020-10-23 11:09:12 -0400

    wsgi fix

--- PPUC/PPUC/wsgi.py
+++ PPUC/PPUC/wsgi.py
@@ -21,7 +21,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'main.settings')
+    'PPUC.settings')
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
