fdb4a368966f899eb0df949ff87abb6d945ef0b0
rwc27 <rwc27@pitt.edu>
2020-11-03 17:57:44 -0500

    added script to automate all db setup

---
+++ PPUC/scripts/setup_app.py
@@ -0,0 +1,16 @@
+from django.core.management import call_command
+
+print("running migrate")
+call_command('migrate')
+print("migrate complete\n\n")
+print("running makemigrations")
+call_command('makemigrations')
+print("makemigrations complete\n\n")
+print("running migrate --run-syncdb")
+call_command('migrate','--run-syncdb')
+print("migrate --run-syncdb complete\n\n")
+print("running runscript add_contracts")
+print("THIS MAY TAKE AWHILE")
+call_command('runscript', 'add_contracts')
+print("rundcript add_contracts complete\n\n")
+call_command('createsuperuser')
