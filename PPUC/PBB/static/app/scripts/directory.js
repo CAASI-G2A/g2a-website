a6e4a9be54f2a5d5d995e93bcaea402b1a43d01d
ktdemay <ktdemay@comcast.net>
2020-12-15 12:51:22 -0500

    Directory - Added scroll, sort arrows, more filter options

--- PPUC/PBB/static/app/scripts/directory.js
+++ PPUC/PBB/static/app/scripts/directory.js
@@ -6,48 +6,20 @@ $(document).ready(function(){
 		});
 	});
 
-	$('#dirTable').DataTable( {
+	var dir = $('#dirTable').DataTable( {
 		"searching": false,
 		"paging": false,
-		"bInfo": false
+		"bInfo": false,
+		"scrollY": "500px",
+		columnDefs: [{
+			orderable: false,
+			targets: 1
+		}]
 	});
 	$('.dataTables_length').addClass('bs-select');
+	dir.columns.adjust().draw();
 });
 
-// function sortTable(n) {
-// 	var table, rows, switching, x, y, shouldSwitch, dir, switchcount = 0;
-// 	table = document.getElementById("dirTable");
-// 	switching = true;
-// 	dir = "asc";
-// 	while (switching) {
-// 		switching = false;
-// 		rows = table.rows;
-// 		for (var i = 1; i < (rows.length - 1); i++) {
-// 		  shouldSwitch = false;
-// 		  x = rows[i].getElementsByTagName("TD")[n];
-// 		  y = rows[i + 1].getElementsByTagName("TD")[n];
-// 		  if (dir == "asc") {
-// 		    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-// 		      shouldSwitch = true;
-// 		      break;
-// 		    }
-// 		  } else if (dir == "desc") {
-// 		    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-// 		      shouldSwitch = true;
-// 		      break;
-// 		    }
-// 		  }
-// 		}
-// 		if (shouldSwitch) {
-// 		  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-// 		  switching = true;
-// 		  switchcount ++;
-// 		} 
-// 		else {
-// 		  if (switchcount == 0 && dir == "asc") {
-// 		    dir = "desc";
-// 		    switching = true;
-// 		  }
-// 		}
-// 	}
-// }
+function filter() {
+	$("#filterBox" > input)
+}
