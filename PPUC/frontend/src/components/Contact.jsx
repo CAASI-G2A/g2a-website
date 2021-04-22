abd370d8b88c96e72f1aec70739aff89dec88e5e
Yongsu Ahn <yong8@me.com>
2021-04-22 17:28:07 -0400

    Captone team work

--- PPUC/frontend/src/components/Contact.jsx
+++ PPUC/frontend/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Input, Form, FormGroup, Label, Button } from "reactstrap";
 
 class Contact extends Component {
   render() {
@@ -30,6 +31,99 @@ class Contact extends Component {
           </address>
           <hr className="my-4 border-top border-secondary" />
         </div>
+        <div class="contact-email-wrapper row jumbotron">
+          <div class="contact-img-wrapper col-md-5 text-center">
+            <img
+              src="/static/app/img/contact_us.jpg"
+              alt="Contact Us"
+              width="90%"
+            />
+          </div>
+          <div class="col-md-7">
+            <Form>
+              <FormGroup>
+                <Label for="firstName">First Name</Label>
+                <Input placeholder="First Name" />
+              </FormGroup>
+              <br />
+              <FormGroup>
+                <Label for="lastName">Last Name</Label>
+                <Input placeholder="Last Name" />
+              </FormGroup>
+              <br />
+              <FormGroup>
+                <Label for="state">State</Label>
+                <Input type="select" name="selectMulti" id="selectState">
+                  <option value="pennsylvania">Pennsylvania</option>
+                  <option value="new jersey">New Jersey</option>
+                  <option value="ohio">Ohio</option>
+                  <option value="florida">Florida</option>
+                  <option value="kentucky">Kentucky</option>
+                  <option value="california">California</option>
+                  <option value="texas">Texas</option>
+                  <option value="new york">New York</option>
+                  <option value="wisconsin">Wisconsin</option>
+                  <option value="arizona">Arizona</option>
+                  <option value="delaware">Delaware</option>
+                  <option value="illionois">Illionois</option>
+                  <option value="north carolina">North Carolina</option>
+                  <option value="south carolina">South Carolina</option>
+                </Input>
+              </FormGroup>
+              <br />
+              <FormGroup>
+                <Label for="message">Message</Label>
+                <Input
+                  className="contact-message"
+                  type="textarea"
+                  name="message"
+                  id="message"
+                />
+              </FormGroup>
+              <Button className="contact-email-button">Submit</Button>
+            </Form>
+          </div>
+        </div>
+
+        {/* <div className="contactSection">
+            <div className="contactCol">
+              <img src="contactus.jpg" alt="Contact Us" width={300} height={350} /> 
+            </div>
+            <div className="contactCol">
+              <form action="/action_page.php">
+                <label htmlFor="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your first name here.." />
+                <label htmlFor="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name here.." />
+                <label htmlFor="State">State</label>
+                <select id="state" name="state">
+                  <option value="pennsylvania">Pennsylvania</option>
+                  <option value="new jersey">New Jersey</option>
+                  <option value="ohio">Ohio</option>
+                  <option value="florida">Florida</option>
+                  <option value="kentucky">Kentucky</option>
+                  <option value="california">California</option>
+                  <option value="texas">Texas</option>
+                  <option value="new york">New York</option>
+                  <option value="wisconsin">Wisconsin</option>
+                  <option value="arizona">Arizona</option>
+                  <option value="delaware">Delaware</option>
+                  <option value="illionois">Illionois</option>
+                  <option value="north carolina">North Carolina</option>
+                  <option value="south carolina">South Carolina</option>
+                </select>
+                <label htmlFor="country">Message</label>
+                <label htmlFor="subject">.</label>
+                <textarea id="subject" name="subject" placeholder="Leave your message" style={{height: '170px'}} defaultValue={""} />
+                <input type="submit" defaultValue="Submit" />
+                <button type="submit">Submit</button>
+                <button type=" button" value="delete"
+                onclick="deleteSomething()">Delete</button>
+                <script>
+              </form>
+            </div>
+          </div>
+        </div> */}
       </div>
     );
   }
--- PPUC/frontend/src/components/Researchers.jsx
+++ PPUC/frontend/src/components/Researchers.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import * as d3 from "d3";
+import d3tooltip from "d3-tooltip";
 import QueryString from "query-string";
 import * as scrollToElement from "scroll-to-element";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,6 +9,7 @@ import Api from "../libs/api";
 import SearchParser from "../libs/researcher_search_lang";
 import routes from "../routes";
 import ResearcherResult from "./ResearcherResult";
+import heatMapData from "../../../PxPUC/static/app/researcher_heatmap_data.json";
 
 class Researchers extends Component {
   constructor(props) {
@@ -28,6 +31,8 @@ class Researchers extends Component {
     this.setSearchQuery = this.setSearchQuery.bind(this);
     this.setStateFilter = this.setStateFilter.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.svg = React.createRef();
+    this.svgHeatmap = React.createRef();
   }
 
   setPage(newPage) {
@@ -145,11 +150,172 @@ class Researchers extends Component {
   }
 
   componentDidMount() {
+    this.renderPieChart();
+    this.renderHeatmap();
     const queryParams = QueryString.parse(this.props.location.search);
     // if search already set, use it
     if (queryParams.search) {
       this.setSearchQuery(queryParams.search, true);
     }
+  }
+
+  renderPieChart() {
+    const width = 250,
+      height = 250,
+      margin = 10;
+
+    let canvas = d3
+      .select(this.svg.current)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    console.log("canvas: ", canvas);
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin;
+
+    // Data here
+    var data = [
+      {
+        category: "Gives officers Unfair Access to Information 17.5%",
+        ratio: 17.5,
+      },
+      { category: "Restricts/Delays Interrogations 20%", ratio: 20 },
+      { category: "Limits Oversight/Discipline 11.9%", ratio: 11.9 },
+      { category: "Erases misconduct Record 11.8%", ratio: 11.8 },
+      { category: "Disqualifies Complaints 2.8%", ratio: 2.8 },
+    ];
+
+    var color = d3
+      .scaleOrdinal()
+      .domain(data.map((d) => d.category))
+      .range(d3.schemeSet2);
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie().value(function (d) {
+      return d.ratio;
+    });
+    var data_ready = pie(data);
+    // Now I know that group A goes from 0 degrees to x degrees and so on.
+
+    // shape helper to build arcs:
+    var arcGenerator = d3.arc().innerRadius(0).outerRadius(radius);
+
+    console.log("data_ready: ", data_ready);
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    canvas
+      .selectAll(".slice")
+      .data(data_ready)
+      .enter()
+      .append("path")
+      .attr("class", "slice")
+      .attr("d", arcGenerator)
+      .attr("fill", function (d) {
+        return color(d.data.category);
+      })
+      .attr("stroke", "black")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7);
+
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    canvas
+      .selectAll(".slice_text")
+      .data(data_ready)
+      .enter()
+      .append("text")
+      .attr("class", "slice_text")
+      .text(function (d) {
+        return d.data.category;
+      })
+      .attr("transform", function (d) {
+        return "translate(" + arcGenerator.centroid(d) + ")";
+      })
+      .style("text-anchor", "middle")
+      .style("font-size", 13);
+  }
+
+  renderHeatmap() {
+    const tooltip = d3tooltip(d3);
+
+    const width = 250,
+      height = 250,
+      margin = 10;
+
+    let canvas = d3
+      .select(this.svgHeatmap.current)
+      .append("g")
+      .attr("transform", "translate(" + 100 + "," + margin * 3 + ")");
+
+    const data = heatMapData;
+
+    // Build X scales and axis:
+    var x = d3
+      .scaleBand()
+      .range([0, width])
+      .domain(data.map((d) => d.City))
+      .padding(0.05);
+
+    canvas
+      .append("g")
+      .style("font-size", 12)
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickSize(0))
+      .select(".domain")
+      .remove();
+
+    // Build Y scales and axis:
+    var y = d3
+      .scaleBand()
+      .range([height, 0])
+      .domain(data.map((d) => d.Problem))
+      .padding(0.05);
+
+    var yAxis = canvas
+      .append("g")
+      .style("font-size", 12)
+      .call(d3.axisLeft(y).tickSize(0));
+
+    yAxis.selectAll("text").attr("transform", "translate(-5,0)rotate(-45)");
+
+    yAxis.select(".domain").remove();
+
+    // Build color scale
+    var myColor = d3
+      .scaleSequential()
+      .interpolator(d3.interpolateInferno)
+      .domain([1, 100]);
+
+    // add the squares
+    canvas
+      .selectAll()
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", function (d) {
+        return x(d.City);
+      })
+      .attr("y", function (d) {
+        return y(d.Problem);
+      })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("width", x.bandwidth())
+      .attr("height", y.bandwidth())
+      .style("fill", function (d) {
+        return myColor(d.Contracts);
+      })
+      .style("stroke-width", 4)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+      .on("mouseover", (d, i) => {
+        const htmlForInput = "<div># of contracts: " + d.Contracts + "</div>";
+        tooltip.html(htmlForInput);
+        tooltip.show();
+      })
+      .on("mouseout", (d, i) => {
+        tooltip.hide();
+      });
   }
 
   render() {
@@ -225,6 +391,182 @@ class Researchers extends Component {
               >
                 investigation
               </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12 mt-3">
+          <div class="col-lg-12 d-flex">
+            <div class="problematic-language-intro jumbotron">
+              <h2 class="w3-padding-32">
+                Police union contracts make it more difficult to hold police
+                officers accountable for misconduct.
+              </h2>
+              <p class="w3-text-grey">
+                Police union contracts contain key information on the internal
+                processes of police bureaus. However, they are often hard to
+                find and contain confusing language. There needs to be more
+                transparency in these documents and what they actually say. We
+                aim to not only provide actual contract documents but also to
+                highlight critical problematic phrasing that limits
+                accountability in police unions.
+              </p>
+              <div class="jumbotron d-flex">
+                <p class="col-md-7">
+                  <p class="w3-text-grey">
+                    <i>
+                      "No police officer shall be compelled by the City to be
+                      interviewed by and/or to testify before the Citizen Police
+                      Review Board. Any statements compelled by the City under
+                      the Garrity Rule during internal investigations are
+                      confidential and not subject to discovery or release to
+                      the Citizens Police Review Board." (Sec 21 C)
+                    </i>
+                  </p>
+
+                  <p class="w3-text-grey">
+                    <b>Translation:</b> the city does not require officers to
+                    cooperate with the CPRB during the complaint process and
+                    they keep their internal review completely confidential from
+                    the review board.
+                  </p>
+                </p>
+                <p class="col-md-5">
+                  <img
+                    src="/static/app/img/researcher_introduction.jpg"
+                    width="100%"
+                  />
+                </p>
+              </div>
+              <p class="w3-text-grey">
+                Policing in the US is highly fragmented with more than 100
+                police departments in Allegheny County alone. Contracts are
+                opaque and negotiated borough by borough, leading to confusion
+                and distrust in local government.
+              </p>
+            </div>
+          </div>
+
+          <div class="problematic-language-stat jumbotron d-flex">
+            <div class="col-md-8">
+              <h3 class="w3-padding-32">Complaint Statistics</h3>
+              <p class="w3-text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div class="col-md-4">
+              <svg
+                className="svg-pie-chart"
+                ref={this.svg}
+                width={"100%"}
+                height={300}
+              />
+            </div>
+          </div>
+          <div class="problematic-language-category jumbotron">
+            <h3 class="w3-padding-32">Problematic Provisions</h3>
+            <table>
+              <tr>
+                <th>Problematic Provision</th>
+                <th>Definition</th>
+              </tr>
+              <tr>
+                <td>
+                  Delays Interrogations of Officers Suspected of Misconduct
+                </td>
+                <td>
+                  The contract includes any stipulation that delays officer
+                  interviews or interrogations after alleged wrongdoing for a
+                  set length of time (for exmaple, two days or twenty-four
+                  hours).
+                </td>
+              </tr>
+              <tr>
+                <td>Provides Access to Evidence Before Interview</td>
+                <td>
+                  The contract provides officers with access to evidence before
+                  interviews or interrogations about alleged wrongdoing (for
+                  example, complete investigative files or statements from other
+                  witnesses).
+                </td>
+              </tr>
+              <tr>
+                <td>Limits Consideration of Disciplinary History</td>
+                <td>
+                  The contract mandates the destruction or purging of
+                  disciplinary records from the personnel files after a set
+                  length of time, or limits the consideration of disciplinary
+                  records in future employment actions.
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Limits Length of Investigation or Establishes Statute of
+                  Limitations
+                </td>
+                <td>
+                  The contract prohibits the interrogation, investigation, or
+                  punishment of officers on the basis of alleged wrongdoing if
+                  too much time has elapes since its alleged occurence, or since
+                  the initiation of the investigation.
+                </td>
+              </tr>
+              <tr>
+                <td>Limits Anonymous Complaints</td>
+                <td>
+                  The contract prohibits supervisors from interrogating,
+                  investigating, or discplining officers on the basis of
+                  anonymous civilian complaints.
+                </td>
+              </tr>
+              <tr>
+                <td>Limits Civilian Oversight</td>
+                <td>
+                  The contract prohibits civilian groups from acquiring the
+                  authority to investigate, discpline or terminate officers for
+                  alleged wrongdoing.
+                </td>
+              </tr>
+              <tr>
+                <td>Permits or Requires Arbitration</td>
+                <td>
+                  The contract permits or requires arbitration of disputes
+                  related to disciplinary penalites or termination.
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div class="problematic-language-stat jumbotron d-flex">
+            <div class="col-md-8">
+              <h3 class="w3-padding-32">
+                Problematic Provisions by categories and states
+              </h3>
+              <p class="w3-text-grey">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div class="col-md-4">
+              <svg
+                className="svg-heatmap"
+                ref={this.svgHeatmap}
+                width={350}
+                height={300}
+              />
             </div>
           </div>
         </div>
