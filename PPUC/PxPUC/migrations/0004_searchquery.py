1104f7c3e82ada0c9163775816b5172f34fcdcb0
halimousine <andihalim@berkeley.edu>
2021-10-12 15:22:16 +0000

    Push modified migration

--- PPUC/PxPUC/migrations/0004_searchquery.py
+++ PPUC/PxPUC/migrations/0004_searchquery.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('PxPUC', '0003_glossaryterm'),
+        ('PxPUC', '0005_glossaryterm'),
     ]
 
     operations = [
