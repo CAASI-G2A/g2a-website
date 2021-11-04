748414937647094a77333a3115104d7ba32d1d95
griffincj <cjg95@pitt.edu>
2021-11-04 11:30:52 -0400

    Update Question and Answer fields for FAQ

--- PPUC/PxPUC/models.py
+++ PPUC/PxPUC/models.py
@@ -2,19 +2,31 @@
 Definition of models.
 """
 
-from django.db import models
+from django.db import models, reset_queries
 
-# Create your models here.
+# Location object is the foundation for all other objects, most refer to it
 class Location(models.Model):
     name = models.CharField(max_length=150, default="")
     state = models.CharField(max_length=50, default="")
-    complaint_form_link = models.URLField(max_length=300)
-
+    complaint_form_link = models.URLField(max_length=300, null=True)
+    # Region (string)
+    # Police Budget (if available, float)
+    # Number of officers (if available, int)
     def __str__(self):
         return self.name
 
+# Keyword table is a junction table of all keywords for Location. Each Location may have (1-5?) keywords
+# and each Keyword may belong to more than one Location (but are separate entries!)
+# class Keyword
+    # location (foreign key)
+    # category (foreign key)
+    # word (string)
 
+
+# Corresponds to the handful of Campaign Zero categories. These are mostly static, and referred to by 
+# each keyword object
 class Category(models.Model):
+    # Should be separate fields for category_name, and category_description
     category = models.CharField(max_length=300)
 
     def __str__(self):
@@ -23,8 +35,8 @@ class Category(models.Model):
 
 class Question(models.Model):
     category = models.ManyToManyField(Category, related_name="questions")
-    q = models.CharField(max_length=450, default="")
-    a = models.CharField(max_length=450, default="")
+    q = models.CharField(max_length=1000, default="")
+    a = models.CharField(max_length=1000, default="")
     location = models.ForeignKey(
         Location, related_name="questions", on_delete=models.CASCADE, null=True
     )
@@ -54,7 +66,14 @@ class Sentence(models.Model):
     def __str__(self):
         return self.text
 
+# Junction table with single entry per keyword per sentence
+# For example: If sentence #1 contains two keywords, will be entry for Setence 1, Key 1, and Sentence 1, Key 2
+# class Sentence_Keyword
+    # sentence (foreign key)
+    # keyword (foreign key)
 
+
+# May need to be modified instead of adding a new class
 class Problematic_Sentence(models.Model):
     location = models.ForeignKey(
         Location, related_name="problematic_sentences", on_delete=models.CASCADE
@@ -71,7 +90,7 @@ class Problematic_Sentence(models.Model):
     def __str__(self):
         return self.text
 
-
+# Also deprecated, replaced by category and keyword
 class GlossaryTerm(models.Model):
     location = models.ForeignKey(
         Location, related_name="glossary_terms", on_delete=models.CASCADE
@@ -79,7 +98,7 @@ class GlossaryTerm(models.Model):
     term = models.TextField()
     definition = models.TextField()
 
-
+# Used for saving user search queries
 class SearchQuery(models.Model):
     query = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
