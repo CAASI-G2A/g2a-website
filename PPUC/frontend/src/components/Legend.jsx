1b51425c1bb7cd6690ce3ac8b479933ca9894e6f
Yongsu Ahn <yong8@me.com>
2021-11-17 11:36:56 -0500

    Added instruction and data dictionary on map; Added reading guide on contract detail page; Display "No info" when no data available

--- PPUC/frontend/src/components/Legend.jsx
+++ PPUC/frontend/src/components/Legend.jsx
@@ -10,22 +10,22 @@ function Legend({ map }) {
         const div = L.DomUtil.create("div", "info legend");
         let labels = [];
         labels.push(
-          '<i style = "background: rgba(255, 255, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> NH (North Hills)'
+          '<i style = "background: rgba(255, 255, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> North Hills'
         );
         labels.push(
-          '<i style = "background: rgba(128, 0, 128, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> AA (Airport Area)'
+          '<i style = "background: rgba(128, 0, 128, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> Airport Area'
         );
         labels.push(
-          '<i style = "background: rgba(96, 96, 96, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> PGH (Pittsburgh)'
+          '<i style = "background: rgba(96, 96, 96, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> Pittsburgh'
         );
         labels.push(
-          '<i style = "background: rgba(0, 255, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> ES (East Suburb)'
+          '<i style = "background: rgba(0, 255, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> East Suburb'
         );
         labels.push(
-          '<i style = "background: rgba(255, 165, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> SH (South Hills)'
+          '<i style = "background: rgba(255, 165, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> South Hills'
         );
         labels.push(
-          '<i style = "background: rgba(255, 0, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> MV (Mon Valley)'
+          '<i style = "background: rgba(255, 0, 0, 0.3)">&nbsp;&nbsp;&nbsp;&nbsp</i> Mon Valley'
         );
 
         div.innerHTML = labels.join("<br>");
--- PPUC/frontend/src/components/SmallList.jsx
+++ PPUC/frontend/src/components/SmallList.jsx
@@ -268,6 +268,18 @@ class SmallList extends Component {
             </Radio.Button>
           </Radio.Group>
         </div>
+        <div style={{ padding: 10, color: 'gray', fontStyle: 'italic' }}>
+          Below is a map of Allegheny County, PA. Above the map are some keywords from 
+          police contracts that  may relate to police accountability, 
+          such as disqualification of misconduct complaints or destruction of misconduct records. 
+          Read more about these keywords <a target="_blank" href="https://www.grieftoaction.org/#/commentary">here</a>.
+          <br/>What you can do:
+          <ul>
+            <li>Click on a region on the map OR in the panel to view information about the police department in a particular borough/township/municipality. </li>
+            <li>Click on any of the keywords to see the boroughs where they appear. </li>
+          </ul>
+        </div>
+        <div style={{ fontStyle: 'italic', color: 'gray', fontSize: '0.8rem' }}>*For more information in the tooltip, refer to <a target="_blank" href="https://docs.google.com/spreadsheets/d/1jAnGHnQdK9UZK_Iy9fxkdfIlat7krILq/edit#gid=1063952290">link</a>.</div>
         <div className="map_list_wrapper">
           <div className="map_wrapper leaflet-container leaflet-touch leaflet-retina leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom">
             <MapComponent
