ad04ac5d3082d114a033d641689be558b8dbc03c
griffincj <cjg95@pitt.edu>
2021-11-18 10:13:29 -0500

    Re-fix views.py for Location object error on FAQ page

--- PPUC/PxPUC/views.py
+++ PPUC/PxPUC/views.py
@@ -315,8 +315,6 @@ class LocationQuestionList(generics.ListAPIView):
     lookup_url_kwarg = "lid"
 
     def get_queryset(self):
-        lid = self.kwargs.get(self.lookup_url_kwarg)
-        location = Location.objects.get(pk=lid)
         queryset = Category.objects.all().prefetch_related(
             Prefetch(
                 "questions",
