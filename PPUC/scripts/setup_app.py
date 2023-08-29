404278c691b769b681cde3fc5d88ffaffb037b43
pgavazzi1 <72482489+pgavazzi1@users.noreply.github.com>
2023-08-29 17:24:24 -0400

    Moved order of setup processes

--- PPUC/scripts/setup_app.py
+++ PPUC/scripts/setup_app.py
@@ -18,37 +18,25 @@ def run():
     call_command('migrate','--run-syncdb')
     print("migrate --run-syncdb complete\n\n")
 
+    #added by ACPP SPRING 2023
+    print('Downloading contracts, this will take a minute or two')
+    call_command('runscript', 'download_contracts')
+    print('contracts downloaded\n\n')
+
     # original setup scripts... to be removed once new setup started in 2023 is finalized
     print("running runscript add_contracts")
     print("THIS MAY TAKE AWHILE - PLEASE BE PATIENT")
     call_command('runscript', 'add_contracts')
     print("runscript add_contracts complete\n\n")
 
-    print("running runscript add_sentences")
-    call_command('runscript', 'add_sentences')
-    print ("runscript add_sentences complete\n\n")
-
-    print("running runscript add_questions")
-    call_command('runscript', 'add_questions')
-    print ("runscript add_questions complete\n\n")
-
-    #added by ACPP SPRING 2023
-    print('Downloading contracts, this will take a minute or two')
-    call_command('runscript', 'download_contracts')
-    print('contracts downloaded\n\n')
-
-    # #added by ACPP SPRING 2023
+    # New setup script... added by ACPP SPRING 2023
+    # Not fully ready yet as of SU23.... still need original setup script as of rn to make Location objects 
     print('Reading in Excel Sheet')
     call_command('runscript', 'read_master_sheet')
     print('master sheet read\n\n')
 
+    # Adds sentences for the setupscript 
+    print("running runscript add_questions")
+    call_command('runscript', 'add_questions')
+    print ("runscript add_questions complete\n\n")
 
-
-
-    # print("running npm install")
-    # subprocess.run('npm install', cwd=FRONTEND_DIR, shell=True)
-    # print ("npm install complete\n\n")
-
-    # print("running npm run build")
-    # subprocess.run('npm run build', cwd=FRONTEND_DIR, shell=True)
-    # print ("npm run build complete\n\n")
